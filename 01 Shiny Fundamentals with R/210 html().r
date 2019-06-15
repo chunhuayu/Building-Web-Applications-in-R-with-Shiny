0ede31bb747c536d42cf5e0dfcc500f84e9d5ef4
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-15 13:11:34 -0400

    Create 210 html().r

---
+++ 01 Shiny Fundamentals with R/210 html().r
@@ -0,0 +1,81 @@
+### Creating and formatting HTML output
+
+# In the previous exercise you developed an app that reported averages of selected x and y variables as two separate outputs. 
+# An alternative approach would be to combine them into a single, multi-line output, using a new render function: renderUI().
+
+### Instructions
+
+# In the server: There is a new output that replaces output$avg_x and output$avg_y from the previous app. 
+# The id of this output should be avgs.
+# Fill in the blanks to achieve the same resulting look as in the previous app -- the two averages displayed on separate lines.
+# In the UI: Replace the textOutputs with one call to htmlOutput, calling the new HTML text string you created in the server.
+
+### R Script
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_4850/datasets/movies.Rdata"))
+
+# UI
+ui <- fluidPage(
+  sidebarLayout(
+    
+    # Input(s)
+    sidebarPanel(
+      
+      # Select variable for y-axis
+      selectInput(inputId = "y",
+                  label = "Y-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                  selected = "audience_score"),
+      
+      # Select variable for x-axis
+      selectInput(inputId = "x",
+                  label = "X-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                  selected = "critics_score")
+      
+    ),
+    
+    # Output(s)
+    mainPanel(
+      plotOutput(outputId = "scatterplot"),
+      htmlOutput(outputId = "avgs"), # avg of x
+      htmlOutput(outputId = "avgs"), # avg of y
+      verbatimTextOutput(outputId = "lmoutput") # regression output
+    )
+  )
+)
+
+# Server
+server <- function(input, output) {
+  
+  # Create scatterplot
+  output$scatterplot <- renderPlot({
+    ggplot(data = movies, aes_string(x = input$x, y = input$y)) +
+      geom_point()
+  })
+
+  # Calculate averages
+  output$avg_x <- renderText({
+    avgs <- movies %>% pull(input$x) %>% mean() %>% round(2)
+    avgs <- movies %>% pull(input$y) %>% mean() %>% round(2)
+    HTML(
+      paste("Average", input$x, "=", avgs),
+      "<br/>",
+      paste("Average", input$y, "=", avgs)
+    )
+  })
+  
+  # Create regression output
+  output$lmoutput <- renderPrint({
+    x <- movies %>% pull(input$x)
+    y <- movies %>% pull(input$y)
+    print(summary(lm(y ~ x, data = movies)), digits = 3, signif.stars = FALSE)
+  })
+  
+}
+
+# Create a Shiny app object
+shinyApp(ui = ui, server = server)
