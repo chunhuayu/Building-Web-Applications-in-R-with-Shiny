a7451b56eff558a42589daf339ffe9332d1e6900
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-15 10:18:49 -0400

    Create 202 Two output plot.r

---
+++ 01 Shiny Fundamentals with R/202 Two output plot.r
@@ -0,0 +1,64 @@
+### where's my plot?
+
+# The server function of this app builds two plots, scatterplot and densityplot, however the app only displays one.
+
+### Instructions
+# Run the app and identify which plot is missing.
+# Make the necessary update to the app UI to display the missing plot.
+# Reduce the height of the new plot using the height argument in the plotOutput function to height = 200.
+
+### R Script
+
+library(shiny)
+library(ggplot2)
+load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_4850/datasets/movies.Rdata"))
+
+# Define UI for application that plots features of movies
+ui <- fluidPage(
+
+  # Sidebar layout with a input and output definitions
+  sidebarLayout(
+
+    # Inputs
+    sidebarPanel(
+
+      # Select variable for y-axis
+      selectInput(inputId = "y",
+                  label = "Y-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                  selected = "audience_score"),
+
+      # Select variable for x-axis
+      selectInput(inputId = "x",
+                  label = "X-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                  selected = "critics_score")
+    ),
+
+    # Outputs
+    mainPanel(
+      plotOutput(outputId = "scatterplot"),
+      plotOutput(outputId = "densityplot", height = 200)
+    )
+  )
+)
+
+# Define server function required to create the scatterplot
+server <- function(input, output) {
+
+  # Create scatterplot
+  output$scatterplot <- renderPlot({
+    ggplot(data = movies, aes_string(x = input$x, y = input$y)) +
+      geom_point()
+  })
+
+  # Create densityplot
+  output$densityplot <- renderPlot({
+    ggplot(data = movies, aes_string(x = input$x)) +
+      geom_density()
+  })
+
+}
+
+# Create the Shiny app object
+shinyApp(ui = ui, server = server)
